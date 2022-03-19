92f80643127948163ae576313051330bea24011f
pticaperepel <92641116+EugeneBazarova@users.noreply.github.com>
2022-03-19 23:14:06 +0300

    cmmt-1

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,4 +1,57 @@
-angular.module('app', []).controller('indexController', function ($scope, $http) {
+angular.module('app', ['ngStorage']).controller('indexController', function ($scope, $http, $localStorage) {
+    $scope.tryToAuth = function () {
+        $http.post('http://localhost:8081/febmarket/auth', $scope.user)
+            .then(function successCallback(response) {
+                if (response.data.token) {
+                    $http.defaults.headers.common.Authorization = 'Bearer ' + response.data.token;
+                    $localStorage.winterMarketUser = {username: $scope.user.username, token: response.data.token};
+
+                    $scope.user.username = null;
+                    $scope.user.password = null;
+                }
+            }, function errorCallback(response) {
+            });
+    };
+
+    $scope.tryToLogout = function () {
+        $scope.clearUser();
+        $scope.user = null;
+    };
+
+    $scope.clearUser = function () {
+        delete $localStorage.winterMarketUser;
+        $http.defaults.headers.common.Authorization = '';
+    };
+
+    $scope.isUserLoggedIn = function () {
+        if ($localStorage.winterMarketUser) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    $scope.authCheck = function () {
+        $http.get('http://localhost:8081/febmarket/auth_check').then(function (response) {
+            alert(response.data.value);
+        });
+    };
+
+    if ($localStorage.winterMarketUser) {
+        try {
+            let jwt = $localStorage.winterMarketUser.token;
+            let payload = JSON.parse(atob(jwt.split('.')[1]));
+            let currentTime = parseInt(new Date().getTime() / 1000);
+            if (currentTime > payload.exp) {
+                console.log("Token is expired!!!");
+                delete $localStorage.winterMarketUser;
+                $http.defaults.headers.common.Authorization = '';
+            }
+        } catch (e) {
+        }
+
+        $http.defaults.headers.common.Authorization = 'Bearer ' + $localStorage.winterMarketUser.token;
+    }
     $scope.loadProducts = function () {
         $http.get('http://localhost:8081/febmarket/api/v1/products/').then(function (response) {
             $scope.productsList = response.data;
@@ -29,7 +82,7 @@ angular.module('app', []).controller('indexController', function ($scope, $http)
         });
     }
 
-    //TODO
+
     $scope.removeOneItem = function (productId){
         $http.delete('http://localhost:8081/febmarket/api/v1/cart/remove/'+ productId).then(function (response) {
             $scope.loadCart();
@@ -49,6 +102,19 @@ angular.module('app', []).controller('indexController', function ($scope, $http)
                 $scope.loadCart();
             });
         }
+    }
+
+    $scope.createOrder = function(){
+        $http.post('http://localhost:8081/febmarket/api/v1/orders', $scope.user).then(function (response) {
+            alert("Заказ сформирован");
+            $scope.loadOrders();
+        });
+    }
+
+    $scope.loadOrders = function () {
+        $http.get('http://localhost:8081/febmarket/api/v1/orders').then(function (response) {
+            $scope.orders = response.data;
+        });
     }
 
     $scope.loadProducts();
